37a1ccbc5425304b15c5878ef807c9c84528ecc5
renzy1965 <136796009+renzy1965@users.noreply.github.com>
2025-01-08 13:31:17 +0800

    Godbless.py

--- Godbless.py
+++ Godbless.py
@@ -85,7 +85,7 @@ def load_model():
 
         if not os.path.exists(model_path):
             with st.spinner('Downloading model... Please wait.'):
-                model_url = "https://drive.google.com/file/d/1MC9a-YgAzyCz-4k3WP8wSSfrVPWRFbpT"
+                model_url = "https://drive.google.com/uc?id=1jfV3PIz2dR0_LKEgHZO2cbvRZtFWx65K"
                 gdown.download(model_url, output=model_path, quiet=True)
 
         return tf.keras.models.load_model(model_path)
